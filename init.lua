54e2d82a6f897285a910fbbda311fbd6639c0fb5
Yurii Bilyk <yurii.bilyk@biberk.com>
2026-02-09 18:12:24 +0200

    Set 'gd' as Go-To-Definition LSP command

--- init.lua
+++ init.lua
@@ -234,6 +234,8 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+--
+vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { buffer = bufnr, desc = 'LSP: Go to definition' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
